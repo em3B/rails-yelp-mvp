3c88fe0df9425c983349d6b15a3d31b41c358ad3
Emily Baird <emilym.baird@gmail.com>
2022-01-18 20:44:19 +0000

    both models added, most of the restaurant controller and views worked on, reviews controller and views need to be worked on

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_18_095415) do
+ActiveRecord::Schema.define(version: 2022_01_18_190542) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2022_01_18_095415) do
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "restaurant_id"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Restaurant.create([{ name: "le pain quotidien", address: "1 bank street", category: "belgian" },
+                                 { name: "wagamama", address: "39 jubilee place", category: "japanese" },
+                                 { name: "amerigo vespucci", address: "25 cabot square", category: "italian" },
+                                 { name: "uoichi", address: "122 meridian pl", category: "japanese" },
+                                 { name: "roka", address: "4 park pavillion", category: "japanese" }])
